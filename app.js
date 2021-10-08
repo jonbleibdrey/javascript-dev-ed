068904a041dfabf3350eebcb762a3fc14fc3d564
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-10-08 15:47:15 -0400

    added functions

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var life = 100;
-life = life - 20
-
-var name = "jonathan bleibdrey"
+//functions and parameters
 
 
-function checkout(){
-    var checkout = false;
-    if(checkout){
-        return "wow"
-    } else {
-        return "you have nothing"
-    }
 
+function nameChange(name){
+
+   console.log( name == "jonathan bleibdrey"? "hes the man" : "not a good person")
 }
 
-console.log(checkout());
+nameChange("jonathan bleibdrey")
+
+// varibales and data
+
+// var life = 100;
+// life = life - 20
+
+// var name = "jonathan bleibdrey"
+
+
+// function checkout(){
+//     var checkout = false;
+//     if(checkout){
+//         return "wow"
+//     } else {
+//         return "you have nothing"
+//     }
+
+// }
+
+// console.log(checkout());
