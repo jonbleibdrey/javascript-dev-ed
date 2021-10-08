f3653b4a84e4ef2c01554ba5161e77532fac9381
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-10-08 15:39:14 -0400

    added varibales and if statment

---
+++ app.js
@@ -0,0 +1,17 @@
+var life = 100;
+life = life - 20
+
+var name = "jonathan bleibdrey"
+
+
+function checkout(){
+    var checkout = false;
+    if(checkout){
+        return "wow"
+    } else {
+        return "you have nothing"
+    }
+
+}
+
+console.log(checkout());
